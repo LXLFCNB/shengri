507b98087b9222a41910b7dbb7202e8a0526f6d4
LXL <2712366216@qq.com>
2024-01-15 14:44:14 +0800

    改点图片

--- js/index.js
+++ js/index.js
@@ -9,8 +9,9 @@
 $("#login-button").click(function (event) {
   var userName = document.getElementById("userName").value;
   var pwd = document.getElementById("pwd").value;
+
   //修改密码请改此处
-  if (userName == "123" && pwd == "123") {
+  if (userName == "迟迟" && pwd == "115") {
     event.preventDefault();
     $("form").fadeOut(500);
     $(".wrapper").addClass("form-success");
@@ -18,6 +19,7 @@ $("#login-button").click(function (event) {
       location.href = "BirthdayCake.html";
     }, 2000);
   } else {
-    alert("Wrong Password");
+    alert("密码错误");
   }
 });
+
